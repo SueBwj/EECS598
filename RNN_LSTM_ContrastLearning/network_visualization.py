be3c930b93d01b6217290ce14659810beb82e455
Sue Dickinson <1175634156@qq.com>
2025-02-07 13:26:14 +0800

    HW4

--- RNN_LSTM_ContrastLearning/network_visualization.py
+++ RNN_LSTM_ContrastLearning/network_visualization.py
@@ -15,96 +15,124 @@ from a4_helper import *
 
 
 def hello():
-  """
-  This is a sample function that we will try to import and run to ensure that
-  our environment is correctly set up on Google Colab.
-  """
-  print('Hello from network_visualization.py!')
+    """
+    This is a sample function that we will try to import and run to ensure that
+    our environment is correctly set up on Google Colab.
+    """
+    print('Hello from network_visualization.py!')
+
 
 def compute_saliency_maps(X, y, model):
-  """
-  Compute a class saliency map using the model for images X and labels y.
+    """
+    Compute a class saliency map using the model for images X and labels y.
 
-  Input:
-  - X: Input images; Tensor of shape (N, 3, H, W)
-  - y: Labels for X; LongTensor of shape (N,)
-  - model: A pretrained CNN that will be used to compute the saliency map.
+    Input:
+    - X: Input images; Tensor of shape (N, 3, H, W)
+    - y: Labels for X; LongTensor of shape (N,)
+    - model: A pretrained CNN that will be used to compute the saliency map.
 
-  Returns:
-  - saliency: A Tensor of shape (N, H, W) giving the saliency maps for the input
-  images.
-  """
-  # Make input tensor require gradient
-  X.requires_grad_()
-  
-  saliency = None
-  ##############################################################################
-  # TODO: Implement this function. Perform a forward and backward pass through #
-  # the model to compute the gradient of the correct class score with respect  #
-  # to each input image. You first want to compute the loss over the correct   #
-  # scores (we'll combine losses across a batch by summing), and then compute  #
-  # the gradients with a backward pass.                                        #
-  # Hint: X.grad.data stores the gradients                                     #
-  ##############################################################################
-  # Replace "pass" statement with your code
-  pass
-  ##############################################################################
-  #               END OF YOUR CODE                                             #
-  ##############################################################################
-  return saliency
+    Returns:
+    - saliency: A Tensor of shape (N, H, W) giving the saliency maps for the input
+    images.
+    """
+    # Make input tensor require gradient
+    X.requires_grad_()
+
+    saliency = None
+    ##############################################################################
+    # TODO: Implement this function. Perform a forward and backward pass through #
+    # the model to compute the gradient of the correct class score with respect  #
+    # to each input image. You first want to compute the loss over the correct   #
+    # scores (we'll combine losses across a batch by summing), and then compute  #
+    # the gradients with a backward pass.                                        #
+    # Hint: X.grad.data stores the gradients                                     #
+    ##############################################################################
+    # Replace "pass" statement with your code
+    scores = model.forward(X)
+
+    # 获得正确目标对应的scores
+    target_scores = scores[torch.arange(X.size(0)), y]
+
+    # 反向传播：对每个样本，传入梯度 1.0
+    target_scores.backward(torch.ones_like(target_scores))
+
+    # X.grad 即为输入图像的梯度；取绝对值后在通道维度上取最大值得到显著性图
+    saliency, _ = torch.max(X.grad.abs(), dim=1)  # saliency shape: (N, H, W)
+    ##############################################################################
+    #               END OF YOUR CODE                                             #
+    ##############################################################################
+    return saliency
+
 
 def make_adversarial_attack(X, target_y, model, max_iter=100, verbose=True):
-  """
-  Generate an adversarial attack that is close to X, but that the model classifies
-  as target_y.
+    """
+    Generate an adversarial attack that is close to X, but that the model classifies
+    as target_y.
 
-  Inputs:
-  - X: Input image; Tensor of shape (1, 3, 224, 224)
-  - target_y: An integer in the range [0, 1000)
-  - model: A pretrained CNN
-  - max_iter: Upper bound on number of iteration to perform
-  - verbose: If True, it prints the pogress (you can use this flag for debugging)
+    Inputs:
+    - X: Input image; Tensor of shape (1, 3, 224, 224)
+    - target_y: An integer in the range [0, 1000)
+    - model: A pretrained CNN
+    - max_iter: Upper bound on number of iteration to perform
+    - verbose: If True, it prints the pogress (you can use this flag for debugging)
 
-  Returns:
-  - X_adv: An image that is close to X, but that is classifed as target_y
-  by the model.
-  """
-  # Initialize our adversarial attack to the input image, and make it require gradient
-  X_adv = X.clone()
-  X_adv = X_adv.requires_grad_()
-  
-  learning_rate = 1
-  ##############################################################################
-  # TODO: Generate an adversarial attack X_adv that the model will classify    #
-  # as the class target_y. You should perform gradient ascent on the score     #
-  # of the target class, stopping when the model is fooled.                    #
-  # When computing an update step, first normalize the gradient:               #
-  #   dX = learning_rate * g / ||g||_2                                         #
-  #                                                                            #
-  # You should write a training loop.                                          #
-  #                                                                            #
-  # HINT: For most examples, you should be able to generate an adversarial     #
-  # attack in fewer than 100 iterations of gradient ascent.                    #
-  # You can print your progress over iterations to check your algorithm.       #
-  ##############################################################################
-  # Replace "pass" statement with your code
-  pass
-  ##############################################################################
-  #                             END OF YOUR CODE                               #
-  ##############################################################################
-  return X_adv
+    Returns:
+    - X_adv: An image that is close to X, but that is classifed as target_y
+    by the model.
+    """
+    # Initialize our adversarial attack to the input image, and make it require gradient
+    X_adv = X.clone()
+    X_adv = X_adv.requires_grad_()
+
+    learning_rate = 1
+    ##############################################################################
+    # TODO: Generate an adversarial attack X_adv that the model will classify    #
+    # as the class target_y. You should perform gradient ascent on the score     #
+    # of the target class, stopping when the model is fooled.                    #
+    # When computing an update step, first normalize the gradient:               #
+    #   dX = learning_rate * g / ||g||_2                                         #
+    #                                                                            #
+    # You should write a training loop.                                          #
+    #                                                                            #
+    # HINT: For most examples, you should be able to generate an adversarial     #
+    # attack in fewer than 100 iterations of gradient ascent.                    #
+    # You can print your progress over iterations to check your algorithm.       #
+    ##############################################################################
+    # Replace "pass" statement with your code
+    for i in range(max_iter):
+        if X_adv.grad is not None:
+            X_adv.grad.zero_()
+        model.zero_grad()
+
+        y_hat = model.forward(X_adv)
+        if y_hat.argmax().item() != target_y:
+            target_score = y_hat[0, target_y]
+            target_score.backward()
+
+            dX = learning_rate * X_adv.grad.data / \
+                torch.norm(X_adv.grad.data, p=2, dim=(2, 3), keepdim=True)
+            X_adv = (X_adv + dX).detach().requires_grad_()
+        else:
+            if verbose:
+                print(
+                    f'Iteration {i}: target score {y_hat.argmax()}, max score {target_y}')
+            break
+    ##############################################################################
+    #                             END OF YOUR CODE                               #
+    ##############################################################################
+    return X_adv
 
 
 def class_visualization_step(img, target_y, model, **kwargs):
     """
-    Performs gradient step update to generate an image that maximizes the 
+    Performs gradient step update to generate an image that maximizes the
     score of target_y under a pretrained model.
-  
+
     Inputs:
-    - img: random image with jittering as a PyTorch tensor  
+    - img: random image with jittering as a PyTorch tensor
     - target_y: Integer in the range [0, 1000) giving the index of the class
     - model: A pretrained CNN that will be used to generate the image
-    
+
     Keyword arguments:
     - l2_reg: Strength of L2 regularization on the image
     - learning_rate: How big of a step to take
@@ -123,7 +151,16 @@ def class_visualization_step(img, target_y, model, **kwargs):
     # after each step.                                                     #
     ########################################################################
     # Replace "pass" statement with your code
-    pass
+    if img.grad is not None:
+        img.grad.zero_()
+    model.zero_grad()
+    scores = model.forward(img)
+    target_score = scores[0, target_y]
+    target_score.backward()
+    dX = learning_rate * img.grad.data / \
+        torch.norm(img.grad.data, p=2, dim=(2, 3), keepdim=True)
+    dX -= 2 * l2_reg * img.grad.data
+    img.data += dX
     ########################################################################
     #                             END OF YOUR CODE                         #
     ########################################################################
--- RNN_LSTM_ContrastLearning/style_transfer.py
+++ RNN_LSTM_ContrastLearning/style_transfer.py
@@ -7,23 +7,25 @@ import torch
 import torch.nn as nn
 from a4_helper import *
 
+
 def hello():
-  """
-  This is a sample function that we will try to import and run to ensure that
-  our environment is correctly set up on Google Colab.
-  """
-  print('Hello from style_transfer.py!')
+    """
+    This is a sample function that we will try to import and run to ensure that
+    our environment is correctly set up on Google Colab.
+    """
+    print('Hello from style_transfer.py!')
+
 
 def content_loss(content_weight, content_current, content_original):
     """
     Compute the content loss for style transfer.
-    
+
     Inputs:
     - content_weight: Scalar giving the weighting for the content loss.
     - content_current: features of the current image; this is a PyTorch Tensor of shape
       (1, C_l, H_l, W_l).
     - content_target: features of the content image, Tensor with shape (1, C_l, H_l, W_l).
-    
+
     Returns:
     - scalar content loss
     """
@@ -31,7 +33,12 @@ def content_loss(content_weight, content_current, content_original):
     # TODO: Compute the content loss for style transfer.                         #
     ##############################################################################
     # Replace "pass" statement with your code
-    pass
+    _, C, _, _ = content_current.shape
+    content_current = content_current.reshape(1, C, -1)
+    content_original = content_original.reshape(1, C, -1)
+    content_loss = content_weight * \
+        torch.sum((content_current - content_original)**2, dim=(1, 2))
+    return content_loss
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
@@ -40,13 +47,13 @@ def content_loss(content_weight, content_current, content_original):
 def gram_matrix(features, normalize=True):
     """
     Compute the Gram matrix from features.
-    
+
     Inputs:
     - features: PyTorch Tensor of shape (N, C, H, W) giving features for
       a batch of N images.
     - normalize: optional, whether to normalize the Gram matrix
         If True, divide the Gram matrix by the number of neurons (H * W * C)
-    
+
     Returns:
     - gram: PyTorch Tensor of shape (N, C, C) giving the
       (optionally normalized) Gram matrices for the N input images.
@@ -57,7 +64,11 @@ def gram_matrix(features, normalize=True):
     # Don't forget to implement for both normalized and non-normalized version   #
     ##############################################################################
     # Replace "pass" statement with your code
-    pass
+    N, C, H, W = features.shape
+    features = features.reshape(N, C, -1)
+    gram = torch.bmm(features, features.transpose(1, 2))
+    if normalize:
+        gram /= H * W * C
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
@@ -67,7 +78,7 @@ def gram_matrix(features, normalize=True):
 def style_loss(feats, style_layers, style_targets, style_weights):
     """
     Computes the style loss at a set of layers.
-    
+
     Inputs:
     - feats: list of the features at every layer of the current image, as produced by
       the extract_features function.
@@ -78,7 +89,7 @@ def style_loss(feats, style_layers, style_targets, style_weights):
       layer style_layers[i].
     - style_weights: List of the same length as style_layers, where style_weights[i]
       is a scalar giving the weight for the style loss at layer style_layers[i].
-      
+
     Returns:
     - style_loss: A PyTorch Tensor holding a scalar giving the style loss.
     """
@@ -89,7 +100,13 @@ def style_loss(feats, style_layers, style_targets, style_weights):
     # You will need to use your gram_matrix function.                            #
     ##############################################################################
     # Replace "pass" statement with your code
-    pass
+    style_loss = 0.0
+    for i, layer in enumerate(style_layers):
+        A_gram = style_targets[i]
+        G_gram = gram_matrix(feats[layer])
+        style_loss += style_weights[i] * \
+            torch.sum((A_gram - G_gram)**2)
+    return style_loss
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
@@ -98,11 +115,11 @@ def style_loss(feats, style_layers, style_targets, style_weights):
 def tv_loss(img, tv_weight):
     """
     Compute total variation loss.
-    
+
     Inputs:
     - img: PyTorch Variable of shape (1, 3, H, W) holding an input image.
     - tv_weight: Scalar giving the weight w_t to use for the TV loss.
-    
+
     Returns:
     - loss: PyTorch Variable holding a scalar giving the total variation loss
       for img weighted by tv_weight.
@@ -112,7 +129,13 @@ def tv_loss(img, tv_weight):
     # Your implementation should be vectorized and not require any loops!        #
     ##############################################################################
     # Replace "pass" statement with your code
-    pass
+    tv_loss = 0.0
+    _, _, H, W = img.shape
+    diff_h = img[:, :, 1:, :] - img[:, :, :H-1, :]
+    # 计算水平方向上相邻像素的差异：从第 1 列到最后一列与前一列的差值
+    diff_w = img[:, :, :, 1:] - img[:, :, :, :W-1]
+    tv_loss += tv_weight * (torch.sum(diff_h**2) + torch.sum(diff_w**2))
+    return tv_loss
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
